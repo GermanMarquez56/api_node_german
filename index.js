7dad7b561113ad326a780f2699fb152e5921033c
germanmarquez56 <germanmarquez17@gmai.com>
2025-07-09 18:48:54 -0500

    09/07

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express=require('express'); //importamos la libreria
 
 const app=express()//instanciando una aplicacion tipo express
 
+
+//esto permite leer un json que llegue por body
+app.use(express.json())
+
 //req: request
 //res: response
 //ruta
@@ -25,24 +29,85 @@ let usuarios=[
 ]
 
 //ruta para obtener el listado de usuarios
-app.get("/usuarios",(req,res)=>{
+app.get("/usuarios/todos",(req,res)=>{
     res.json(usuarios)
 });
 
 //obtener usuarios por id
 
-app.get("/usuarios/:id",(req,res)=>{
+app.get("/usuarios/buscar/:id",(req,res)=>{
     const id = parseInt(req.params.id);
     //console.log(typeof(id))
     const usuario=usuarios.find(user=>user.id===id)
+    //console.log(usuario)
+    if(!usuario){
+        res.status(404).json({
+            mensaje:"usuario no encontrado"
+        })
+    }
     res.json(usuario)
 })
 
+//crear un nuevo usuario CREATE
+app.post("/usuarios/crear", (req, res)=>{
+    //console.log(req.body)
+    const {nombre, email}=req.body; //define la variable
+    //console.log(nombre, email)
+    const nuevoUsuario={
+        id:usuarios.length+1,
+        nombre:nombre,
+        email:email
+    }
+    //guardar el objeto creado en la BBDD
+    usuarios.push(nuevoUsuario)
+    res.status(201).json({
+        mensaje:"usuario creado correctamente",
+        usuarioCreado:nuevoUsuario
+    })
+})
 
+//actualizar un usuario UPDATE
+app.put("/usuarios/actualizar/:id",(req, res)=>{
+    const {nombre, email}=req.body;
+    const id=parseInt(req.params.id);
+    //buscar
+    const usuario=usuarios.find(u=>u.id===id);
+    if(!usuario){
+        res.status=(404).json({
+            mensaje:"usuario a modificar no encontrado"
+        })
+    }
+    const infoAnterior=usuario; //guardo los datos encontrados
+    usuario.nombre=nombre; //modifica el campo nombre
+    usuario.email=email
+    
+    res.status(202).json({
+        mensaje:"usuario modificado correctamente",
+        infoAnterior:infoAnterior,
+        infoNueva:usuario
+    })
 
+})
 
+//DELETE
+app.delete("/usuarios/eliminar/:id",(req, res)=>{
+//capturar el id pasado por parametros
+const id=parseInt(req.params.id);
+//encontrar el indice correspondiente al id entregado
+const index=usuarios.findIndex(user=>user.id===id)
+//eliminar de la lista la info del indice encontrado
+//console.log(index)
+if(index===-1){
+    res.status(404).json({
+        mensaje:`usuario con id ${id} no encontrado`
+    })
+}else{
+    usuarios.splice(index)
+    res.status(200).json({
+    mensaje:`usuario con id ${id} eliminado correctamente`
+})
 
-
+})
 
 
 
